458577652084c1dcc11debf66fdb9c10208e227a
Aditya <adityaa00098@gmail.com>
2025-12-09 14:33:31 +0530

    Setup basic express server with env

---
+++ server/server.js
@@ -0,0 +1,16 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("This is home Page");
+});
+
+// take port from env
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log("Server is running at", PORT);
+});
