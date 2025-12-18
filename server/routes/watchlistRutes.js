6e75c3ccd5f1cef46a334e0b6b3745f772a877ed
Aditya <adityaa00098@gmail.com>
2025-12-18 13:31:25 +0530

    some backend modfication with cors

--- server/routes/watchlistRutes.js
+++ server/routes/watchlistRutes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const Watchlist = require("../models/watchlist");
-const authMiddleware = require("../middleware/authMiddleware");
+const authMiddleware = require("../midlleware/authMiddleware");
 
 
 // Add a watchlist 
--- server/server.js
+++ server/server.js
@@ -1,15 +1,23 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
-const connectDb = require("./config/database")
+const cors = require("cors");
+
+const connectDb = require("./config/database");
 const authRoutes = require("./routes/authRoutes");
 const stockRoutes = require("./routes/stockRoutes");
 const watchlistRutes = require("./routes/watchlistRutes");
 
 app.use(express.json());
+
+app.use(cors({
+  origin: "http://localhost:5173",
+  credentials: true
+}));
+
 connectDb();
 
-app.use("/api/auth",authRoutes);
+app.use("/api/auth", authRoutes);
 app.use("/api/stocks", stockRoutes);
 app.use("/api/watch", watchlistRutes);
 
@@ -17,9 +25,7 @@ app.get("/", (req, res) => {
   res.send("This is home Page");
 });
 
-// take port from env
 const PORT = process.env.PORT || 3000;
-
 app.listen(PORT, () => {
   console.log("Server is running at", PORT);
 });
